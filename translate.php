50c93982fd7db6ecbacd643b65e88f5da7faa5f7
Jannick Fahlbusch <git@jf-projects.de>
2015-09-13 22:46:57 +0200

    Complete the information for the help-plugin

--- translate.php
+++ translate.php
@@ -5,6 +5,12 @@ use Stichoza\GoogleTranslate\TranslateClient;
 
 class translate extends Script
 {
+	protected $helpMessage = "'translate from LANGUAGECODE to LANGUAGECODE STRING'\n"
+							."'translate from LANGUAGECODE to LANGUAGECODE STRING'\n"
+							."'tr LANGUAGECODE - LANGUAGECODE STRING'\n"
+							."You can use 'AUTO' as LANGUAGECODE to detect the language of the given string.";
+	protected $description = 'Translates a given string';
+
 	public function run()
 	{
 		$tr = new TranslateClient($this->matches[1], $this->matches[2]);
